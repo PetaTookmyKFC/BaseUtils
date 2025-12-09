80afa143592615b9a71370c9a7e501aec6d37922
PetaTookmyKFC <36772620+PetaTookmyKFC@users.noreply.github.com>
2025-12-09 18:12:01 +0000

    Removed Pointers

--- include/BaseUtils/types/BaseVectors.h
+++ include/BaseUtils/types/BaseVectors.h
@@ -10,15 +10,15 @@ namespace base_utils::types
     template<typename T>
     struct Vector2
     {
-        T* x,y;
-        Vector2(T* x, T* y) : x(x), y(y) {}
+        T x,y;
+        Vector2(T x, T y) : x(x), y(y) {}
     };
 
     template<typename T>
     struct Vector3
     {
-        T* x,y,z;
-        Vector3(T* x, T* y, T* z) : x(x), y(y), z(z) {}
+        T x,y,z;
+        Vector3(T x, T y, T z) : x(x), y(y), z(z) {}
     };
 
 }
